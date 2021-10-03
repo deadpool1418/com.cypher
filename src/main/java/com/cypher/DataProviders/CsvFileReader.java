3031a84a7dc2924fc5e5d33b3bea69984f80a4fd
Vijay <vachaudhari26@gmail.com>
2021-10-03 10:04:10 +0530

    Readers added for csv and xlsx files

--- src/main/java/com/cypher/DataProviders/CsvFileReader.java
+++ src/main/java/com/cypher/DataProviders/CsvFileReader.java
@@ -15,12 +15,21 @@ public class CsvFileReader {
 	public static CSVReader csvReader;
 
 	public static Iterator<Object[]> readDataFromCsv(String filePath) throws IOException {
-		file = new File(filePath);
-		csvReader = new CSVReader(new FileReader(file));
-		String[] nextRecord;
-		List<Object[]> data = new ArrayList<Object[]>();
-		while ((nextRecord = csvReader.readNext()) != null) {
-			data.add(nextRecord);
+		int i = 0;
+		List<Object[]> data = null;
+		try {
+			file = new File(filePath);
+			csvReader = new CSVReader(new FileReader(file));
+			String[] nextRecord;
+			data = new ArrayList<Object[]>();
+			while ((nextRecord = csvReader.readNext()) != null) {
+				if(i != 0)
+					data.add(nextRecord);
+				i += 1;
+			}
+		}
+		catch (Exception e) {
+			System.out.println("File not found !!!");
 		}
 		return data.iterator();
 
--- src/main/java/com/cypher/DataProviders/ExcelFileReader.java
+++ src/main/java/com/cypher/DataProviders/ExcelFileReader.java
@@ -2,97 +2,55 @@ package com.cypher.DataProviders;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ExcelFileReader {
-	XSSFWorkbook excelWorkbook = null;
-	XSSFSheet excelSheet = null;
-	XSSFRow row = null;
-	XSSFCell cell = null;
-	public Object[][] getData() throws IOException 
+
+	public static Object[][] readDataFromExcel(String filePath)
 	{
-		FileInputStream fis = new FileInputStream("/home/cypher/Documents/data.xlsx"); // Your .xlsx file name along with path
-		excelWorkbook = new XSSFWorkbook(fis);
-		// Read sheet inside the workbook by its name
-		excelSheet = excelWorkbook.getSheet("Sheet1"); //Your sheet name
-		// Find number of rows in excel file
-		System.out.println("First Row Number/index:"+ excelSheet.getFirstRowNum() + " *** Last Row Number/index:"
-				+ excelSheet.getLastRowNum());
-		int rowCount = excelSheet.getLastRowNum() - excelSheet.getFirstRowNum()+1;
-		int colCount = excelSheet.getRow(0).getLastCellNum();
-		System.out.println("Row Count is: " + rowCount
-				+ " *** Column count is: " + colCount);
-		Object data[][] = new Object[rowCount-1][colCount];
-		for (int rNum = 2; rNum <= rowCount; rNum++) 
-		{
-			for (int cNum = 0; cNum < colCount; cNum++) 
-			{
-				System.out.print(getCellData("Sheet1", cNum, rNum) + " "); // Your sheet name
-				data[rNum - 2][cNum] = getCellData("Sheet1", cNum, rNum); //Your sheet name
+		File file;
+		FileInputStream fileInputStream;
+		XSSFWorkbook workbook;
+		Object data[][] = null;
+		try {
+			file = new File(filePath);	
+			fileInputStream = new FileInputStream(file);
+			workbook = new XSSFWorkbook(fileInputStream);
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			int rowN = sheet.getLastRowNum() - sheet.getFirstRowNum() + 1;
+			int colN = sheet.getRow(0).getLastCellNum();
+			data = new Object[rowN - 1][colN];
+			int r = 1, c = 0;
+			System.out.println(rowN);
+			while(r<rowN) {
+				c = 0;
+				while(c<colN) {
+					System.out.println(r + " " + c);
+					data[r - 1][c] = getCellData(sheet.getRow(r).getCell(c));
+					c += 1;
+				}
+				r += 1;
 			}
-			System.out.println();
+			workbook.close();
+			
+		}
+		catch (Exception exception) {
+			exception.printStackTrace();
 		}
 		return data;
-	}
-	// Function will always used as below. It returns the data from a cell - No need to make any changes
-	public String getCellData(String sheetName, int colNum, int rowNum) 
-	{
-		try
-		{
-			if (rowNum <= 0)
-				return "";
-			int index = excelWorkbook.getSheetIndex(sheetName);
-			if (index == -1)
-				return "";
-			excelSheet = excelWorkbook.getSheetAt(index);
-			row = excelSheet.getRow(rowNum - 1);
-			if (row == null)
-				return "";
-			cell = row.getCell(colNum);
-			if (cell == null)
-				return "";
-			System.out.println(cell.getClass());
-			if (cell.getCellType() == Cell.CELL_TYPE_STRING)
-				return cell.getStringCellValue();
-			else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC
-					|| cell.getCellType() == Cell.CELL_TYPE_FORMULA)
-			{
-				String cellText = String.valueOf(Integer.parseInt(cell.getStringCellValue()));
-				System.out.println(Integer.parseInt(cell.getStringCellValue()));
-				return cellText;
-			} else if (cell.getCellType() == Cell.CELL_TYPE_BLANK)
-				return "";
-			else
-				return String.valueOf(cell.getBooleanCellValue());
-		} catch (Exception e)
-		{
-			e.printStackTrace();
-			return "row " + rowNum + " or column " + colNum
-					+ " does not exist in xls";
-		}
-	}
-
-	public static void main(String[] args) throws IOException {
-		File file = new File("/home/cypher/Documents/data.xlsx");
-		FileInputStream fileInputStream = new FileInputStream(file);
-		XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
-		XSSFSheet sheet = workbook.getSheetAt(0);
 		
-		Iterator<Row> rowIterator = sheet.iterator();
-		while(rowIterator.hasNext()) {
-			Row row = rowIterator.next();
-			Iterator<Cell> cellIterator = row.iterator();
-			while(cellIterator.hasNext()) {
-				Cell cell = cellIterator.next();
-				System.out.println(cell.getNumericCellValue());
-			}
+	}
+	
+	public static String getCellData(Cell cell) {
+		if(cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+			return String.valueOf((int)cell.getNumericCellValue());
 		}
+		else if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
+			return cell.getStringCellValue();
+		}
+		return "";
 	}
 }
